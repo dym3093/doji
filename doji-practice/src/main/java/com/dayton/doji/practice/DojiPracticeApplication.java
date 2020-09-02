eb0510d40486bb7a55f05f6f9cd3483fe2978778
dengyouming <dengyouming@nokia.com>
2020-09-02 21:35:44 +0800

    1. 修改测试提交。

--- doji-practice/src/main/java/com/dayton/doji/practice/DojiPracticeApplication.java
+++ doji-practice/src/main/java/com/dayton/doji/practice/DojiPracticeApplication.java
@@ -3,6 +3,11 @@ package com.dayton.doji.practice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 入口类
+ * @author Martin Deng
+ * @since 2020/9/2 21:35
+ */
 @SpringBootApplication
 public class DojiPracticeApplication {
 
